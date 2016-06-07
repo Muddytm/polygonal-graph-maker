f93c638a210e6b3e3eb13a64aa0529acf7beb511
calebh93 <hawkins.caleb93@gmail.com>
2016-06-07 12:17:25 -0700

    Working boysssss

--- create.py
+++ create.py
@@ -1,27 +1,36 @@
 from PIL import Image
 from PIL import ImageDraw
 import json
+import math
 
 
 def create(values):
     """Create graph."""
-    im = Image.open("assets/template.png")  # Some sort of background
+    im = Image.open("assets/template2.jpg")  # Some sort of background
     draw = ImageDraw.Draw(im)
+
+    angle = 360. / len(values)
 
     origin = (im.size[0]/2, im.size[1]/2)
 
-    for value in list(range(len(values))):
-        point = origin
-        new_point = (origin[0] - values[value], origin[1])
-        print (point)
-        print (new_point)
-        draw.line((point, new_point), fill="blue", width=10)
-        break
+    points = []
+
+    for i in list(range(len(values))):
+        cur_angle = i * angle
+        width = math.cos(math.radians(cur_angle)) * values[i] * 20
+        height = math.sin(math.radians(cur_angle)) * values[i] * 20
+        points.append((origin[0] - width, origin[1] - height))
+
+    for i in list(range(len(points))):
+        if i != (len(points) - 1):
+            draw.line((points[i], points[i + 1]), fill=128, width=5)
+        else:
+            draw.line((points[i], points[0]), fill=128, width=5)
 
     del draw
 
-    im.save("example.jpg")
+    im.save("example.png")
 
 
 if __name__ == "__main__":
-    create([4, 6, 2, 8, 5, 9, 1, 3, 7])
+    create([2, 8, 5, 3, 9, 10, 4, 1, 10, 5, 3, 9, 7, 4, 6, 5, 10, 9])
